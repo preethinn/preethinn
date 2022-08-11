d41f995c48eaf4536861e8225f1d149b1b1da63b
Preethi N <96244557+preethinn@users.noreply.github.com>
2022-08-11 21:36:42 +0530

    Add files via upload
    To create String and find the sum of its length

---
+++ prob1.java
@@ -0,0 +1,24 @@
+package Sessionone;
+
+public class Problem1 {
+	public static void main(String args[]) {
+		//char to String
+		char name[] = {'a','d','a','m'};//char type 1d array
+		String firstName = new String(name);
+		
+		//String object
+		String lastName = new String( "edward");
+		//System.out.println(new String("adam").length() + new String("edward").length());
+		
+		//Sum of length of 2 strings
+		System.out.println("Sum of 2 string lengths:"+(firstName.length()+lastName.length()));
+		
+		
+		System.out.println(firstName.replace("adam" , "Adam")+ " "+ lastName.replace("e","E"));
+		
+		
+		int expression = firstName.compareTo(lastName);
+		System.out.println(expression < 0 ? "YES" : "NO");
+	}
+
+}
