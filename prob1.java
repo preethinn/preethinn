a0c98e88f157314fdaee530b45ddf242e3dd0dce
Preethi N <96244557+preethinn@users.noreply.github.com>
2022-08-21 22:43:35 +0530

    Update prob1.java

--- prob1.java
+++ prob1.java
@@ -7,15 +7,16 @@ public class Problem1 {
 		String firstName = new String(name);
 		
 		//String object
-		String lastName = new String( "edward");
+		String lastName = new String("edward");
 		//System.out.println(new String("adam").length() + new String("edward").length());
 		
 		//Sum of length of 2 strings
-		System.out.println("Sum of 2 string lengths:"+(firstName.length()+lastName.length()));
+		System.out.println("Sum of "+firstName+" and "+lastName+" strings lengths:"+(firstName.length()+lastName.length()));
 		
 		
-		System.out.println(firstName.substringreplace("adam" , "Adam")+ " "+ lastName.replace("e","E"));
-		
+		System.out.println(firstName.substring(0,1).
+	   toUpperCase()+firstName.substring(1)+" "+lastName.substring(0,1).toUpperCase()+lastName.substring(1));
+		 
 		
 		int expression = firstName.compareTo(lastName);
 		System.out.println(expression < 0 ? "YES" : "NO");
