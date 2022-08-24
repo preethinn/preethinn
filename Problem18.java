e1ed6c88a34f3a07e809920312e3b72f7e388a87
Preethi N <96244557+preethinn@users.noreply.github.com>
2022-08-24 21:10:04 +0530

    Update Problem18.java

--- Problem18.java
+++ Problem18.java
@@ -1,28 +1,24 @@
-//Problem 18: Reverse a Number and Palindrome
+//Problem 18: Reverse a Number
+
 package Sessiontwo;
-import java.util.Scanner;
-public class Palindrome {
-	public static void main(String args[])
+import java.util.Scanner;// built-in util package which includes several classes among them scanner class is being used in this program
+
+public class ReverseANumber
+{
+	public static void main(String args[]) 
 	{
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the Number to check palindrome or not:");
-		int number = scan.nextInt();
-		int temp = number;
+		Scanner scan = new Scanner(System.in); //Scanner class's Object (scan) got created
+		System.out.println("Enter the Number:");
+		int inputNumber = scan.nextInt();//123
+		
 		int reverse = 0;
-		 while (number > 0)
-		 {
-		int remainder = number % 10;//123 % 10 = remainder = 3 | 12%10 = remainder = 2 | 1%10 = 1
-		reverse = reverse * 10 + remainder; // 3|32|321
-		number = number /10; //123/10 = 12 |12/10 = 1 | 1/10 = 0
+		while (inputNumber > 0)
+		{
+		//To get last digit
+		int remainder = inputNumber % 10;//3
+		reverse = reverse * 10 + remainder; //3
+		inputNumber = inputNumber/10;//123/10 = 12
 		}
-		 System.out.println(reverse);
-			if(reverse == temp)
-			{
-				System.out.println("Its a Palindrome");
-			}
-			else
-			{
-				System.out.println("Its Not a Palindrome");
-			}	 
-	}
+		System.out.println("The Reverse of Entered Number "+inputNumber+" is "+reverse);
+	}		
 }
